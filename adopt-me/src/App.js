990b93d0be9e1eca092fe34353ea0c6570bc5d04
Marc Byndas <byndasma@gmail.com>
2017-11-03 02:21:36 +0000

    App.js

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -5,34 +5,32 @@ const pf = petfinder(credentials)
 
 class App extends React.Component {
   constructor(props) {
-      super(props)
-      this.state = {
-        animal: 'dog',
-        breed: 'Havanese',
-        location: 'San Francisco, CA',
-        pets: []
-      }
+    super(props)
+    this.state = {
+      animal: 'dog',
+      breed: 'Havanese',
+      location: 'San Francisco, CA',
+      pets: []
     }
-    componentDidMount() {
-      const { animal, breed, location } = this.state
-      const promise = pf.pet.find({ animal, breed, location, output: 'full' })
-      promise.then((data) => {
-        const pets = data.petfinder.pets ? data.petfinder.pets.pet : []
-        this.setState({ pets })
-      });
-    }
-    render() {
-      return (
-        <div className='app'>
+  }
+  componentDidMount() {
+    const { animal, breed, location } = this.state
+    const promise = pf.pet.find({ animal, breed, location, output: 'full' })
+    promise.then((data) => {
+      const pets = data.petfinder.pets ? data.petfinder.pets.pet : []
+      this.setState({ pets });
+    });
+  }
+  render() {
+    return (
+      <div className='app'>
         <img src='src/adopt-me.png' alt='adopt-me logo' />
         <div>
           <pre><code>{JSON.stringify(this.state, null, 4)}</code></pre>
         </div>
       </div>
-      )
-    }
+    )
+  }
 }
-
-export default App
 
 export default App
